9ff77dbc5289457707e01ab9d97dd8a90d57d659
hououinkami <hououinkami@gmail.com>
2021-04-12 16:29:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,5 +6,7 @@ cd package
 git clone https://github.com/tty228/luci-app-serverchan.git
 wget https://raw.githubusercontent.com/hououinkami/ServerChan/master/serverchan
 mv -f serverchan luci-app-serverchan/root/usr/bin/serverchan/
+sed -i 's/微信推送/设备状态推送/g' luci-app-serverchan/luasrc/controller/serverchan.lua
+
 #
 cd ./
