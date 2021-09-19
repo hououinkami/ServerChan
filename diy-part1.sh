3ceb04061162372891ec28d81ab41652902aa872
hououinkami <hououinkami@gmail.com>
2021-09-19 13:19:01 +0800

    更换serverchan源

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,6 +3,10 @@ cd package
 # 自定义插件
 #
 # 编译ServerChan
+cd lean
+rm -rf luci-app-serverchan
+cd -
+
 git clone https://github.com/tty228/luci-app-serverchan.git
 
 sed -i 's/微信推送/设备状态推送/g' luci-app-serverchan/luasrc/controller/serverchan.lua
